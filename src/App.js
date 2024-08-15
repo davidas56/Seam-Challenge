95479361ef4304224796e3f793a397e59c9992e8
davidas <davidas362@gmail.com>
2024-08-15 18:33:36 -0500

    adding more styles

--- src/App.js
+++ src/App.js
@@ -9,12 +9,25 @@ function App() {
           <a className="menu" style={{
               padding: "20px",
               textalign: "center",
-              background: "#1abc9c",
+              background: "#black",
               color: "white",
-              fontsize: "30px"
+              fontsize: "30px",
+              textalign: "center",
+              fontsize: "35px",
+              fontfamily: "system-ui",
           }}>
             Seams
           </a>
+        </div>
+        <div className="EmotionVideo" style={{
+          padding: "18%",
+        }}>
+          <p> here comos the camerea if u want delete this this is only a guide</p>   
+        </div>
+        <div className="FooterCapture" style={{
+          padding: "2%"
+        }}>
+          <bottom> aaa</bottom>
         </div>
       </header>
     </div>
