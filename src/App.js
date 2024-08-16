faa6ea3898ae9502d8d6262228c0c7f00070a28d
davidas <davidas362@gmail.com>
2024-08-15 23:40:31 -0500

    react camera libraries

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Webcam from "react-webcam";
+
+const WebcamComponent = () => <Webcam />;
 
 function App() {
   return (
